6a4fd6b087e8f8ae2c72e1bcd4add8d9e77fd2f2
yusuf1kose <yusuf.kose@sjsu.edu>
2024-11-19 16:52:28 -0800

    Update data.js

--- src/data.js
+++ src/data.js
@@ -1,17 +1,34 @@
-const weatherData = [];
+const weatherData = {
+  "San Jose": [],
+  "Milpitas": [],
+  "Pleasanton": [],
+  "Los Gatos": [],
+  "Santa Clara": []
+};
 
-function collectData() {
+function collectData(city) {
+  const tempRanges = {
+    "San Jose": [15, 30],
+    "Milpitas": [14, 28],
+    "Pleasanton": [12, 25],
+    "Los Gatos": [13, 27],
+    "Santa Clara": [16, 32]
+  };
+
+  const [min, max] = tempRanges[city];
   const newData = {
-    temperature: (Math.random() * 40).toFixed(1), // Simulated temperature
-    humidity: (Math.random() * 100).toFixed(1),    // Simulated humidity
+    temperature: (Math.random() * (max - min) + min).toFixed(1), // Simulated temperature
+    chanceOfRain: (Math.random() * 30).toFixed(1), // Chance of rain (0-30%)
+    chanceOfSnow: (Math.random() * 10).toFixed(1), // Chance of snow (0-10%)
     timestamp: new Date().toISOString()
   };
-  weatherData.push(newData);
+
+  weatherData[city].push(newData);
   return newData;
 }
 
-function getData() {
-  return weatherData;
+function getData(city) {
+  return weatherData[city] || [];
 }
 
 module.exports = { collectData, getData };

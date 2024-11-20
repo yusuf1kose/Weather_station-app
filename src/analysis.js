c9debcc4817323ba7a51488cc1c9ab530adb5ec7
yusuf1kose <yusuf.kose@sjsu.edu>
2024-11-19 16:52:08 -0800

    Update analysis.js

--- src/analysis.js
+++ src/analysis.js
@@ -1,7 +1,6 @@
 function calculateAverageTemperature(data) {
-    const total = data.reduce((sum, entry) => sum + parseFloat(entry.temperature), 0);
-    return (total / data.length).toFixed(1);
-  }
-  
-  module.exports = { calculateAverageTemperature };
-  
+  const total = data.reduce((sum, entry) => sum + parseFloat(entry.temperature), 0);
+  return (total / data.length).toFixed(1);
+}
+
+module.exports = { calculateAverageTemperature };
